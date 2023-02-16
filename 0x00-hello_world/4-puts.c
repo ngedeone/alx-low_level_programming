f074bd202ade3417847d535ab0445a2fe579aa15
ngedeone <ochiengwycliff122@gmail.com>
2023-02-16 13:00:46 -0800

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-i#include <stdio.h>
+#include <stdio.h>
+
 /**
  * main - A program that prints a line with puts function
- * Return: (0) (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\" Programming is like building a multilingual puzzle \n");
-retun(0);
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
 }
