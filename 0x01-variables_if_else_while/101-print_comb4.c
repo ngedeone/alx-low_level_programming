9f6b4a8cd7ee52fb59c6353ef3e8d400a2f38f85
ngedeone <ochiengwycliff122@gmail.com>
2023-02-17 16:28:07 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				if (i != 0 || j != 1 || k != 2)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
