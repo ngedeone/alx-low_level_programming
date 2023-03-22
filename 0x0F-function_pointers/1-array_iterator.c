ec99cabef88114a22edf6f38b552a3f173d337b2
ngedeone <ochiengwycliff122@gmail.com>
2023-03-22 12:44:05 +0300

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include <stddef.h> /* for size_t */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
