8cefe31b9f380f32eb4e256db5b99bf2c4e2c94e
ngedeone <ochiengwycliff122@gmail.com>
2023-03-23 23:47:19 -0700

    Correction to task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,18 +15,16 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	if (n == 0)
-        {
-                return (0);
-        }
+	{
+		return (0);
+	}
 
-        va_start(list, n);
+	va_start(list, n);
 
-        for (i = 0; i < n; i++)
-        {
-                sum += va_arg(list, int);
-        }
-        va_end(list);
-
-        return (sum);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
 }
-
