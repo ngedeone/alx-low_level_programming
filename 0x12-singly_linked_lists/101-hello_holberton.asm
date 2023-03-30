615b0c40d96a80dad32a577c2b2426995bd463cf
ngedeone <ochiengwycliff122@gmail.com>
2023-03-30 10:20:55 -0700

    6. Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,9 @@
-section .data
-	msg db "Hello, Holberton", 0
-	fmt db "%s\n", 0
-
-section .text
-	global main
-	extern printf
-
+global    main
+          extern    printf
 main:
-	push rbx		; save rbx register
-	mov rbx, msg		; load address of message into rbx
-	mov rax, 0		; clear rax register
-	push rax		; push 0 onto stack (for printf's return value)
-	push rbx		; push address of message onto stack (for printf's format string)
-	mov rax, fmt		; load address of format string into rax
-	call printf		; call printf
-	add rsp, 16		; adjust stack pointer to clean up pushed values
-	pop rbx			; restore rbx register
-	xor eax, eax		; return 0
-	ret
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
