c2f24ceffaf9f7b47982deda3310e632d44de089
ngedeone <ochiengwycliff122@gmail.com>
2023-04-04 03:37:46 -0700

    Corrections

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,25 +1,26 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
- * @head: A pointer to a pointer to the head node of the list.
- * @n: The integer to store in the new node.
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: double pointer to the head of the list
+ * @n: integer value to be stored in the new node
  *
- * Return: If memory allocation fails or head is NULL, return NULL.
- *         Otherwise, return the address of the new element.
+ * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	if (head == NULL)
-		return (NULL);
-
+	/* Allocate memory for new node */
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	/* Set the value of the new node */
 	new_node->n = n;
+
+	/* Make the new node the head of the list */
 	new_node->next = *head;
 	*head = new_node;
 
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,34 +1,40 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to a pointer to the head of the list
- * @n: integer to be added to the new node
+ * @head: double pointer to the head of the list
+ * @n: integer value to be stored in the new node
  *
  * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node, *current_node;
+	listint_t *new_node, *temp;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	/* Set the value of the new node */
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	/* If the list is empty, make the new node the head of the list */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    current_node = *head;
-    while (current_node->next != NULL)
-        current_node = current_node->next;
+	/* Traverse the list to find the last node */
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    current_node->next = new_node;
+	/* Make the new node the last node */
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
